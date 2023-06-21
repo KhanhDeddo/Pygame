287db11091c0cae3542bf97a61783e44dbcd9256
KhanhDeddo <129365445+KhanhDeddo@users.noreply.github.com>
2023-06-21 23:25:47 +0700

    Update Euler.py

--- Euler.py
+++ Euler.py
@@ -1,45 +1,15 @@
-#TIM NGICH DAO BANG EULER.
-def gcd(a,m):
-    if m==0:
-        return a
-    return gcd(m,a%m)
-
-def primeFactorization(m):
-    lis=[]
-    for i in range(2,m+1):
-        count=0
-        while m%i==0:
-            count+=1
-            m//=i
-        if count:
-            lis.append(i)
-    return lis
-
-
-def inverseOfA(a,m,lis):
-    s=m
-    for i in lis:#Tinh phi(m).
-        s=s*(1-(1/i))
-    return f"{a}^{int(s-1)}"
-
-
 def binary(b):
-    lis=list(map(int,b.split("^")))#Tach lay so mu.
     base2=""
-    while((lis[1])>0):
-        base2+=str(lis[1]%2)
-        lis[1]//=2
+    while(b>0):
+        base2+=str(b%2)
+        b//=2
     return base2
-
-
 def change(base2):
     lis=[]
     for i in range(len(base2)-1,-1,-1):
         if base2[i]=="1":
-            lis=[i]+lis # chèn số mu cua 2 vào đầu danh sách.
+            lis=[i]+lis 
     return lis
-
-
 def compact(a,m,lst,result,n=0):
     if n in lst:
         result*=a
@@ -49,28 +19,18 @@ def compact(a,m,lst,result,n=0):
 
 def control():
     print("Tim nghich dao cua a theo modulo m. ")
-    print("1. Phuong Phap Euclid mo rong.")
     print("2. Phuong Phap Euler.")
     while True:
         select=int(input("Chon phuong phap tim kiem hoac an 0 de shutdow: "))
         if select==0:
                 print("Chuong trinh da dong.")
                 break
-        if select==1:
-            a=int(eval(input("a = ")))
-            m=int(eval(input("m = ")))
-            inverse(a,m)
         elif select==2:       
             a=int(eval(input("a = ")))
+            b=int(eval(input("b = ")))
             m=int(eval(input("m = ")))
-            if gcd(a,m)==1:
-                result=1 
-                lis=primeFactorization(m)
-                b=inverseOfA(a,m,lis)
-                base2=binary(b)
-                lst=change(base2)
-                print(f"Nghich dao cua {a} theo modulo {m} la: {compact(a,m,lst,result)}\n")
-            else:
-                print(f"{a} khong co kha nghich theo modulo {m}. Vi UCLN({a},{m}) = {gcd(a,m)} != 1\n")
-            
+            result=1;
+            base2=binary(b)
+            lst=change(base2)
+            print(f" la: {compact(a,m,lst,result)}\n")
 control()
